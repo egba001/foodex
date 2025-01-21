ea3791ef79746ecb1c7e56da11f3c93efe0d0ecb
Egba Nnaemeka <egba.nnaemeka@nvit.tech>
2025-01-21 19:00:50 +0100

    header edit

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -58,7 +58,7 @@ export default function Header() {
                             // width="268"
                             height="12"
                             viewBox="0 0 268 12"
-                            className="w-[6rem]"
+                            className="w-[6rem] lg:w-[13rem]"
                             fill="none"
                             xmlns="http://www.w3.org/2000/svg"
                         >
